635ac00d9ccd4a0a60575ced7a4bc665a7464f5a
Daniel Ziltener <dziltener@lyrion.ch>
2013-08-17 02:14:09 +0200

    Added a readme, initial edn.scm commit
    Tokenizer almost complete.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 chicken-edn
 ===========
 
-An EDN reader and writer for chicken scheme.
+An [EDN](https://github.com/edn-format/edn) reader and writer for chicken scheme.
+
+Data type conversions
+---------------------
+
+ * DONE All kinds of numbers get converted to Scheme numbers, precision suffixes (N and M) get ignored.
+ * DONE Keywords :keyword get converted to chicken scheme keywords keyword:.
+ * TODO Maps get converted to a-lists (and vice versa). There'll be a global setting to use srfi-69 hashtables instead.
+ * TODO Vectors are srfi-4 vectors.
+ * TODO The #inst tag requires [egg rfc3339](http://wiki.call-cc.org/eggref/4/rfc3339). You'll have to manually register a handler, but a convenience function is provided.
+ * TODO #uuid is read as a String.
+
+API
+---
+ *TODO*
+
+Other tasks
+-----------
+
+ * Clean up code
+ * Tag handling
+ * Chicken egg
+
+About
+-----
+Written by Daniel Ziltener. EDN written by Rick Hickey. The EDN specification is available at [https://github.com/edn-format/edn](https://github.com/edn-format/edn).
---
+++ edn.scm
@@ -0,0 +1,95 @@
+(require-extension srfi-13)
+(require-extension srfi-1)
+(require-extension irregex)
+
+(define handlers '())
+(define empty-delimiters (list #\,))
+(define list-delimiters (list #\( #\)))
+(define vector-delimiters (list #\[ #\]))
+(define map-delimiters (list #\{ #\}))
+(define seq-delimiters (append list-delimiters vector-delimiters map-delimiters (list)))
+
+(define (register-handler tag fn))
+(define (remove-handler tag))
+
+(define keyword-regex (irregex ""))
+(define (parse-keyword in)
+  (string->keyword (substring/shared in 1 (string-length in))))
+
+(define (parse-character in)
+  (string-concatenate (list "#" in)))
+
+
+(define float-regex (irregex ""))
+(define exact-float-regex (irregex ""))
+(define integer-regex (irregex ""))
+(define arbitrary-integer-regex (irregex ""))
+(define (parse-number in)
+  (string->number
+   (cond ((string-suffix? "N" in) (substring in 0 (- (string-length in) 1)))
+         ((string-suffix? "M" in) (substring in 0 (- (string-length in) 1)))
+         (else in))))
+
+(define (construct-list . in))
+(define (construct-vector . in))
+(define (construct-map . in))
+(define (construct-set . in))
+
+(define (parse-sequence in))
+(define (call/cc=< fn . args)
+  (call/cc (lambda (cc)
+             (apply fn (cons cc args)))))
+
+(define (char-contains? cc c chars)
+  (map (lambda (in) (cond ((char=? c in) (cc #t))
+			  (else in))) chars)
+  (cc #f))
+
+(define (tokenize in)
+  (do [(in-string? #f)
+       (in-number? #f)
+       (in-keyword? #f)
+       (string-cache (list))
+       (digit-cache (list))
+       (keyword-cache (list))
+       (out (list))
+       (next #f)
+       (in (string->list in))]
+      ((null-list? in) (reverse out))
+    (set! next (car in))
+    (set! in (cdr in))
+    (display (string-append "Next char: " (->string next) "\n"))
+    (cond
+     ;; Sequence filling / termination
+     (in-string? (cond ((char=? next #\") (begin (set! in-string? #f)
+                                            (set! out (cons (reverse-list->string string-cache) out))
+                                            (set! string-cache (list))))
+                       (else (set! string-cache (cons next string-cache)))))
+     (in-keyword? (cond
+		   ((call/cc=< char-contains? next (append seq-delimiters (list #\space #\" #\,)))
+		    (begin (set! in-keyword? #f)
+			   (set! out (cons (parse-keyword (reverse-list->string keyword-cache)) out))
+			   (set! keyword-cache (list))
+			   (cond ((call/cc=< char-contains? next seq-delimiters) (set! out (cons next out))))))
+		   (else (set! keyword-cache (cons next keyword-cache)))))
+     
+     (in-number? (cond
+		  ((call/cc=< char-contains? next (append seq-delimiters (list #\space #\" #\,)))
+		   (begin (set! in-number? #f)
+			  (set! out (cons (parse-number (reverse-list->string digit-cache)) out))
+			  (set! digit-cache (list))
+			  (cond ((call/cc=< char-contains? next seq-delimiters) (set! out (cons next out))))))
+		  (else (set! digit-cache (cons next digit-cache)))))
+     ;; Sequence start detection
+     ((char=? next #\") (set! in-string? #t))
+     ((char-numeric? next) (begin (set! in-number? #t)
+                                  (set! digit-cache (cons next digit-cache))))
+     ((char=? next #\:) (begin (set! in-keyword? #t)
+                            (set! keyword-cache (cons next keyword-cache))))
+     ((char-whitespace? next) #f)
+     (else (set! out (cons next out))))))
+
+(define (read-edn-string string))
+(define (read-edn-file file))
+(define (write-edn-string string))
+(define (write-edn-file file))
